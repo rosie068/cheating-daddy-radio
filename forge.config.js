d2cf06ce683c089ebdd29d5363e095dbe035ecbd
rosie068 <rosemaryhe068@gmail.com>
2025-07-09 16:08:30 -0700

    Update forge config for Apple Silicon compatibility

--- forge.config.js
+++ forge.config.js
@@ -38,7 +38,7 @@ module.exports = {
             },
         },
         {
-            name: '@electron-forge/maker-dmg',
+            name: '@electron-forge/maker-zip',
             platforms: ['darwin'],
         },
         {
